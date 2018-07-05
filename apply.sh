a3f58acd34df1d71cebda6582010950df961e8ec
peccu <peccul@gmail.com>
2018-07-05 19:06:17 +0000

    add applyer

---
+++ apply.sh
@@ -0,0 +1,6 @@
+docker-compose exec web /go/src/github.com/syou6162/go-active-learning-web/go-active-learning-web apply --listname github
+docker-compose exec web /go/src/github.com/syou6162/go-active-learning-web/go-active-learning-web apply --listname slideshare
+docker-compose exec web /go/src/github.com/syou6162/go-active-learning-web/go-active-learning-web apply --listname speakerdeck
+docker-compose exec web /go/src/github.com/syou6162/go-active-learning-web/go-active-learning-web apply --listname twitter
+docker-compose exec web /go/src/github.com/syou6162/go-active-learning-web/go-active-learning-web apply --listname arxiv
+docker-compose exec web /go/src/github.com/syou6162/go-active-learning-web/go-active-learning-web apply --listname general
